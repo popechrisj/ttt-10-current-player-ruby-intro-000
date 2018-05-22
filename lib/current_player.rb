e71d12fd95af673361e6bed08d1bd5a969e08434
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-22 20:11:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,7 +7,7 @@ def turn_count(board)
   end
   turn
 end
-
+"""
 def current_player(board)
   player = "X"
   if turn_count(board) % 2 == 0
@@ -16,3 +16,8 @@ def current_player(board)
     player = "O"
   end
 end
+"""
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end 
